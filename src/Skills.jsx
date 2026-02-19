3c9c677a4fc05c1be2c9f64816d752882e8c30a4
Joaquin Ugarte <ugartejoa12@gmail.com>
2026-02-19 15:22:08 -0300

    agregando svg a las skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,14 +1,19 @@
+import {
+  HTML5, CSS, Java, JavaScript,
+  MySQL, Nodejs, Bootstrap, React, TailwindCSS
+} from "./svg/svg";
+
 function Skills() {
   const skills = [
-    { name: "HTML", level: 90 },
-    { name: "CSS", level: 85 },
-    { name: "Java", level: 50 },
-    { name: "JavaScript", level: 70 },
-    { name: "MySQL", level: 45 },
-    { name: "Node JS", level: 35 },
-    { name: "Bootstrap", level: 40 },
-    { name: "React", level: 45 },
-    { name: "Tailwind", level: 60 },
+    { name: "HTML", icon: HTML5 },
+    { name: "CSS", icon: CSS },
+    { name: "Java", icon: Java },
+    { name: "JavaScript", icon: JavaScript },
+    { name: "MySQL", icon: MySQL },
+    { name: "Node JS", icon: Nodejs },
+    { name: "Bootstrap", icon: Bootstrap },
+    { name: "React", icon: React },
+    { name: "Tailwind CSS", icon: TailwindCSS },
   ];
 
   return (
@@ -23,14 +28,10 @@ function Skills() {
           >
             <p className="text-lg font-semibold mb-2 text-sky-700">{skill.name}</p>
 
-            <div className="w-full bg-gray-700 rounded-full h-4 relative overflow-hidden">
-              <div
-                className="absolute top-0 left-0 h-4 bg-gradient-to-r from-blue-400 to-blue-600 rounded-full transition-all duration-700"
-                style={{ width: `${skill.level}%` }}
-              ></div>
+            <div className="mt-4 flex justify-center">
+              {skill.icon({ width: 50, height: 50 })}
             </div>
 
-            <p className="text-sm  mt-2">{skill.level}%</p>
           </div>
         ))}
       </div>
